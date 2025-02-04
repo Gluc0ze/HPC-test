d9004a0f5252de1db05eb557a4338073e9a37aa3
gluc0ze <guillaume.vermeire@hotmail.com>
2025-02-04 21:21:10 +0100

    Initial Commit

---
+++ HPC_git_test/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  HPC_git_test
+//
+//  Created by guillaume vermeire on 04/02/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
