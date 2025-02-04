a3e895d68b5bdc53646c338705378372c6513b27
gluc0ze <guillaume.vermeire@hotmail.com>
2025-02-04 21:49:31 +0100

    Testing v1

--- HPC_git_test/main.cpp
+++ HPC_git_test/main.cpp
@@ -7,8 +7,10 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char * argv[])
+{
+    int repo_test = 0;
     // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "This is a test, HELLO WORLD!\n";
     return 0;
 }
